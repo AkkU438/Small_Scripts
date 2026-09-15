46f970c7a346b6b069d4aecbbc171f7562c42a97
AkkU438 <109703098+AkkU438@users.noreply.github.com>
2026-09-14 21:01:57 -0400

    smth

--- CLI_todo.py
+++ CLI_todo.py
@@ -4,3 +4,22 @@ from datetime import datetime
 
 todo = []
 todo_file = "todo.pkl"
+
+
+# I don't really feel like coding right now, so I'm just gonig to just do some simple stuff that I don't know if I'm going to use.
+
+def display_todo(): 
+    if len(todo) == 0:
+        print("no tasks todo please enter a task")
+    else: 
+        for i, task in enumerate(todo):
+            print(f"{i + 1}, {task}")
+    return
+
+def functionaltiy():
+
+    return 
+
+def main(): 
+    display_todo()
+    return 
